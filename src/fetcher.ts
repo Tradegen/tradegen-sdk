0253e1250d6193d0d6cad681fac9d6829d35103a
Xavier Negron <xaviernegron888@gmail.com>
2021-07-27 14:31:08 -0500

    Finished tests

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -1,6 +1,5 @@
-import { Contract } from '@ethersproject/contracts'
-import { getNetwork } from '@ethersproject/networks'
-import { getDefaultProvider } from '@ethersproject/providers'
+import { newKit } from '@celo/contractkit'
+import { AbiItem } from 'web3-utils'
 import ERC20 from './abis/ERC20.json'
 import AssetPool from './abis/Pool.json'
 import Settings from './abis/Settings.json'
@@ -23,160 +22,141 @@ export abstract class Fetcher {
 
   /**
    * Fetch information for a given token on the given chain, using the given ethers provider.
-   * @param chainId chain of the token
    * @param address address of the token on the chain
-   * @param provider provider used to fetch the token
    * @param symbol optional symbol of the token
    * @param name optional name of the token
    */
   public static async fetchTokenData(
-    chainId: ChainId,
     address: string,
-    provider = getDefaultProvider(getNetwork(chainId)),
     symbol?: string,
     name?: string
   ): Promise<Token> {
+    const kit = newKit('https://alfajores-forno.celo-testnet.org')
+    let instance = new kit.web3.eth.Contract(ERC20 as AbiItem[], address);
     const parsedDecimals =
-      typeof TOKEN_DECIMALS_CACHE?.[chainId]?.[address] === 'number'
-        ? TOKEN_DECIMALS_CACHE[chainId][address]
-        : await new Contract(address, ERC20, provider).decimals().then((decimals: number): number => {
+      typeof TOKEN_DECIMALS_CACHE?.[ChainId.ALFAJORES]?.[address] === 'number'
+        ? TOKEN_DECIMALS_CACHE[ChainId.ALFAJORES][address]
+        : await instance.methods.decimals().call().then((decimals: number): number => {
             TOKEN_DECIMALS_CACHE = {
               ...TOKEN_DECIMALS_CACHE,
-              [chainId]: {
-                ...TOKEN_DECIMALS_CACHE?.[chainId],
+              [ChainId.ALFAJORES]: {
+                ...TOKEN_DECIMALS_CACHE?.[ChainId.ALFAJORES],
                 [address]: decimals
               }
             }
             return decimals
           })
-    return new Token(chainId, address, parsedDecimals, symbol, name)
+    return new Token(ChainId.ALFAJORES, address, parsedDecimals, symbol, name)
   }
 
   /**
    * Fetch information for a given pool on the given chain, using the given ethers provider.
-   * @param chainId chain of the pool
    * @param address address of the pool on the chain
-   * @param provider provider used to fetch the pool
    * @param managerAddress optional address of the pool's manager
    * @param name optional name of the pool
    */
    public static async fetchPoolData(
-    chainId: ChainId,
     address: string,
-    provider = getDefaultProvider(getNetwork(chainId)),
     managerAddress?: string,
     name?: string
   ): Promise<Pool> {
+    const kit = newKit('https://alfajores-forno.celo-testnet.org')
+    let instance = new kit.web3.eth.Contract(AssetPool as AbiItem[], address);
     const parsedFee =
-      typeof POOL_FEE_CACHE?.[chainId]?.[address] === 'number'
-        ? POOL_FEE_CACHE[chainId][address]
-        : await new Contract(address, AssetPool, provider).getPerformanceFee().then((fee: number): number => {
+      typeof POOL_FEE_CACHE?.[ChainId.ALFAJORES]?.[address] === 'number'
+        ? POOL_FEE_CACHE[ChainId.ALFAJORES][address]
+        : await instance.methods.getPerformanceFee().call().then((fee: number): number => {
             POOL_FEE_CACHE = {
               ...POOL_FEE_CACHE,
-              [chainId]: {
-                ...POOL_FEE_CACHE?.[chainId],
+              [ChainId.ALFAJORES]: {
+                ...POOL_FEE_CACHE?.[ChainId.ALFAJORES],
                 [address]: fee
               }
             }
             return fee
           })
-    return new Pool(chainId, address, parsedFee, managerAddress, name)
+    return new Pool(ChainId.ALFAJORES, address, parsedFee, managerAddress, name)
   }
 
   /**
    * Gets the pool's available funds in USD
-   * @param chainID chain of the pool
    * @param address the pool's address
-   * @param provider the provider to use to fetch the data
    */
    public static async getPoolAvailableFunds(
-    chainID: ChainId,
     address: string,
-    provider = getDefaultProvider(getNetwork(chainID))
-  ): Promise<number> {
-    const balance = await new Contract(address, AssetPool, provider).getAvailableFunds()
+  ): Promise<string> {
+    const kit = newKit('https://alfajores-forno.celo-testnet.org')
+    let instance = new kit.web3.eth.Contract(AssetPool as AbiItem[], address);
+    const balance = await instance.methods.getAvailableFunds().call()
     return balance
   }
 
   /**
    * Gets the user's balance in USD for the given pool
-   * @param chainID chain of the pool
    * @param address the pool's address
    * @param user the address of user to get balance of
-   * @param provider the provider to use to fetch the data
    */
    public static async getUserUSDBalance(
-    chainID: ChainId,
     address: string,
     user: string,
-    provider = getDefaultProvider(getNetwork(chainID))
-  ): Promise<number> {
-    const balance = await new Contract(address, AssetPool, provider).getUSDBalance(user)
+  ): Promise<string> {
+    const kit = newKit('https://alfajores-forno.celo-testnet.org')
+    let instance = new kit.web3.eth.Contract(AssetPool as AbiItem[], address);
+    const balance = await instance.methods.getUSDBalance(user).call()
     return balance
   }
 
   /**
    * Gets the pool's positions and total balance
-   * @param chainID chain of the pool
    * @param address the pool's address
-   * @param provider the provider to use to fetch the data
    */
    public static async getPoolPositionsAndTotal(
-    chainID: ChainId,
     address: string,
-    provider = getDefaultProvider(getNetwork(chainID))
-  ): Promise<[string[], number[], number]> {
-    const [positions, balances, total] = await new Contract(address, AssetPool, provider).getPositionsAndTotal()
-    return [positions, balances, total]
+  ): Promise<[string[], string[], string]> {
+    const kit = newKit('https://alfajores-forno.celo-testnet.org')
+    let instance = new kit.web3.eth.Contract(AssetPool as AbiItem[], address);
+    const data = await instance.methods.getPositionsAndTotal().call()
+
+    return [data['0'], data['1'], data['2']]
   }
 
   /**
-   * Returns the currencies available on the platform for the given chainID
-   * @param chainID chain
-   * @param provider the provider to use to fetch the data
+   * Returns the currencies available on the platform
    */
    public static async getSupportedCurrencies(
-    chainID: ChainId,
-    provider = getDefaultProvider(getNetwork(chainID))
   ): Promise<string[]> {
-    const settingsAddress = chainID == ChainId.ALFAJORES ? SETTINGS_ADDRESS_ALFAJORES : '0x0';
-    const supportedCurrencies = await new Contract(settingsAddress, Settings, provider).getAvailableCurrencies()
+    const kit = newKit('https://alfajores-forno.celo-testnet.org')
+    let instance = new kit.web3.eth.Contract(Settings as AbiItem[], SETTINGS_ADDRESS_ALFAJORES)
+    const supportedCurrencies = await instance.methods.getAvailableCurrencies().call()
     return supportedCurrencies
   }
 
   /**
-   * Returns the address of each pool available for the given chainID
-   * @param chainID chain
-   * @param provider the provider to use to fetch the data
+   * Returns the address of each pool available
    */
    public static async getAvailablePools(
-    chainID: ChainId,
-    provider = getDefaultProvider(getNetwork(chainID))
   ): Promise<string[]> {
-    const poolProxyAddress = chainID == ChainId.ALFAJORES ? POOL_FACTORY_ADDRESS_ALFAJORES : '0x0';
-    const availablePools = await new Contract(poolProxyAddress, PoolFactory, provider).getAvailablePools()
+    const kit = newKit('https://alfajores-forno.celo-testnet.org')
+    let instance = new kit.web3.eth.Contract(PoolFactory as AbiItem[], POOL_FACTORY_ADDRESS_ALFAJORES)
+    const availablePools = await instance.methods.getAvailablePools().call()
     return availablePools
   }
 
   /**
    * Given an array of addresses, fetches the token data for each address
-   * @param chainID chain
    * @param addresses array of token addresses
-   * @param provider the provider to use to fetch the data
    */
    public static async fetchTokensFromAddresses(
-    chainID: ChainId,
     addresses: string[],
-    provider = getDefaultProvider(getNetwork(chainID))
   ): Promise<Token[]> {
     let tokens:Promise<Token>[] = new Array(addresses.length);
     for (var i = 0; i < addresses.length; i++)
     {
-        tokens[i] = this.fetchTokenData(chainID, addresses[i], provider);
+        tokens[i] = this.fetchTokenData(addresses[i]);
     }
     
     let output:Token[] = new Array(addresses.length)
-    Promise.all(tokens).then((results:Token[]) => {
+    await Promise.all(tokens).then((results:Token[]) => {
         output = results
     });
 
@@ -185,23 +165,19 @@ export abstract class Fetcher {
 
   /**
    * Given an array of addresses, fetches the pool data for each address
-   * @param chainID chain
-   * @param provider the provider to use to fetch the data
    * @param addresses array of pool addresses
    */
    public static async fetchPoolsFromAddresses(
-    chainID: ChainId,
-    provider = getDefaultProvider(getNetwork(chainID)),
     addresses: string[]
   ): Promise<Pool[]> {
     let pools:Promise<Pool>[] = new Array(addresses.length);
     for (var i = 0; i < addresses.length; i++)
     {
-        pools[i] = this.fetchPoolData(chainID, addresses[i], provider);
+        pools[i] = this.fetchPoolData(addresses[i]);
     }
     
     let output:Pool[] = new Array(addresses.length)
-    Promise.all(pools).then((results:Pool[]) => {
+    await Promise.all(pools).then((results:Pool[]) => {
         output = results
     });
 
--- test/data.test.ts
+++ test/data.test.ts
@@ -1,20 +1,21 @@
 import { Fetcher } from '../src/fetcher'
-import { ChainId } from '../src/constants'
+import { ChainId, TEST_POOL_ADDRESS_ALFAJORES, TEST_USER_ADDRESS } from '../src/constants'
 import { CELO, UBE, TGEN, POOF, Token } from '../src/entities/token'
+import { Pool } from '../src/entities/pool'
 
-describe('data', () => {
+describe('Token data', () => {
   it('Token', async () => {
-    const token = await Fetcher.fetchTokenData(ChainId.ALFAJORES, CELO[ChainId.ALFAJORES].address) // CELO
-    expect(token.decimals).toEqual(18)
+    const token = await Fetcher.fetchTokenData(CELO[ChainId.ALFAJORES].address) // CELO
+    expect(token.decimals).toEqual("18")
   })
 
   it('Supported currencies', async () => {
-    const supportedCurrencies:string[] = await Fetcher.getSupportedCurrencies(ChainId.ALFAJORES)
+    const supportedCurrencies:string[] = await Fetcher.getSupportedCurrencies()
     expect(supportedCurrencies.length).toEqual(4)
-    expect(supportedCurrencies[0]).toEqual(TGEN[ChainId.ALFAJORES].address)
-    expect(supportedCurrencies[1]).toEqual(CELO[ChainId.ALFAJORES].address)
-    expect(supportedCurrencies[2]).toEqual(UBE[ChainId.ALFAJORES].address)
-    expect(supportedCurrencies[3]).toEqual(POOF[ChainId.ALFAJORES].address)
+    expect(supportedCurrencies[0]).toEqual(CELO[ChainId.ALFAJORES].address)
+    expect(supportedCurrencies[1]).toEqual(UBE[ChainId.ALFAJORES].address)
+    expect(supportedCurrencies[2]).toEqual(POOF[ChainId.ALFAJORES].address)
+    expect(supportedCurrencies[3]).toEqual(TGEN[ChainId.ALFAJORES].address)
   })
 
   it('Token data from array of addresses', async () => {
@@ -24,12 +25,75 @@ describe('data', () => {
     addresses[2] = UBE[ChainId.ALFAJORES].address
     addresses[3] = POOF[ChainId.ALFAJORES].address
 
-    const tokens:Token[] = await Fetcher.fetchTokensFromAddresses(ChainId.ALFAJORES, addresses)
+    const tokens:Token[] = await Fetcher.fetchTokensFromAddresses(addresses)
+
     expect(tokens.length).toEqual(4)
     expect(tokens[0].address).toEqual(TGEN[ChainId.ALFAJORES].address)
     expect(tokens[1].address).toEqual(CELO[ChainId.ALFAJORES].address)
     expect(tokens[2].address).toEqual(UBE[ChainId.ALFAJORES].address)
     expect(tokens[3].address).toEqual(POOF[ChainId.ALFAJORES].address)
   })
+})
 
+describe('Pool data', () => {
+  it('Available pools', async () => {
+    const availablePools:string[] = await Fetcher.getAvailablePools()
+
+    console.log("Available pools:")
+    console.log(availablePools)
+
+    expect(availablePools.length).toBeGreaterThanOrEqual(1)
+    expect(availablePools[0]).toEqual(TEST_POOL_ADDRESS_ALFAJORES)
+  })
+
+  it('Pool data from array of addresses', async () => {
+    let addresses:string[] = new Array(1);
+    addresses[0] = TEST_POOL_ADDRESS_ALFAJORES
+
+    const pools:Pool[] = await Fetcher.fetchPoolsFromAddresses(addresses)
+
+    expect(pools.length).toEqual(1)
+    expect(pools[0].address).toEqual(TEST_POOL_ADDRESS_ALFAJORES)
+  })
+
+  it('Pool positions and total balance', async () => {
+    const [positions, balances, total] = await Fetcher.getPoolPositionsAndTotal(TEST_POOL_ADDRESS_ALFAJORES)
+
+    console.log("Positions, balances, and total:")
+    console.log(positions)
+    console.log(balances)
+    console.log(total)
+
+    expect(positions.length).toBeGreaterThanOrEqual(1)
+    expect(balances.length).toBeGreaterThanOrEqual(1)
+    expect(parseInt(total)).toBeGreaterThanOrEqual(0)
+    expect(parseInt(balances[0])).toBeGreaterThanOrEqual(0)
+  })
+
+  it('Get pool available funds', async () => {
+    const funds:string = await Fetcher.getPoolAvailableFunds(TEST_POOL_ADDRESS_ALFAJORES)
+
+    console.log("Pool's available funds: ")
+    console.log(funds)
+
+    expect(parseInt(funds)).toBeGreaterThanOrEqual(0)
+  })
+
+  it('Get pool available funds', async () => {
+    const funds:string = await Fetcher.getPoolAvailableFunds(TEST_POOL_ADDRESS_ALFAJORES)
+
+    console.log("Pool's available funds: ")
+    console.log(funds)
+
+    expect(parseInt(funds)).toBeGreaterThanOrEqual(0)
+  })
+
+  it('Get user USD balance', async () => {
+    const balance:string = await Fetcher.getUserUSDBalance(TEST_POOL_ADDRESS_ALFAJORES, TEST_USER_ADDRESS)
+
+    console.log("User USD balance in test pool: ")
+    console.log(balance)
+
+    expect(parseInt(balance)).toBeGreaterThanOrEqual(0)
+  })
 })
